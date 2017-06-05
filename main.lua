661ed89201a0c40bcf945d3b8f4d1c004d061376
Xgor <real.xgor@hotmail.com>
2017-06-06 00:36:15 +0200

    Buggy waves

--- main.lua
+++ main.lua
@@ -2,6 +2,8 @@ playerX = 100
 playerY = 300
 playerGravity=0
 rotation = 0
+waves = {}
+waveAmount = 0
 
 function love.load()
 	rotation = 0
@@ -9,14 +11,14 @@ end
 
 function love.update(dt)
 
-
+	updateWave(dt)
 	if love.keyboard.isDown('a') then
 		playerX = playerX - 2
-		rotation =rotation -0.01
+--		rotation =rotation -0.02
 	end
 	if love.keyboard.isDown('d') then
 		playerX = playerX + 2
-		rotation =rotation +0.01
+	--	rotation =rotation +0.02
 	end
 
 	if playerY > getWave(playerX)*50+400 then
@@ -56,10 +58,10 @@ function love.draw()
 
 	love.graphics.translate(playerX, playerY-32)
 	if playerNearWaves() then
-		if -math.sin(getWave(playerX)-getWave(playerX+10))*6 < rotation then
-			rotation = rotation-0.04
+		if -math.sin(getWave(playerX)-getWave(playerX+10))*5 < rotation then
+			rotation = rotation-0.01
 		else
-			rotation = rotation+0.04
+			rotation = rotation+0.01
 		end
 	end
 
@@ -67,22 +69,78 @@ function love.draw()
 	
 	love.graphics.rotate( rotation)
 	love.graphics.setColor(0, 0, 255,255)
-	love.graphics.rectangle("fill",0,0,32,32)
---	love.graphics.rotate(0)
+	love.graphics.rectangle("fill",0-16,0,32,32)
+
+	love.graphics.translate(0,0)
+	love.graphics.rotate( 0)
+	love.graphics.setColor(255, 255, 255,255)
+	if waves[2] ~= nil then 
+		love.graphics.print(waves[3].power,20,20) 
+	end
 end
 
 function love.keypressed( key, scancode, isrepeat )
 	if key == 'w' and playerNearWaves() then
 		playerGravity = -8
 	end
-
+	if key == 'x' then
+		addSplash(300,100)
+	end
 	
 end
 
+function addWave(xPos,power,speed)
+	local wave = {}
+	wave.x = xPos
+	wave.power = power
+	wave.size = power
+	wave.speed = speed
+	table.insert(waves,wave)
+	waveAmount = waveAmount+ 1
+end
+
+function addSplash(xPos,power)
+	addWave(xPos,power/2,0)
+	addWave(xPos,-power,-20)
+	addWave(xPos,-power,20)
+--	waveAmount =6
+end
+
+function lerp(a, b, t)
+	return a + (b - a) * t
+end
+
+function updateWave(dt)
+	for i = 1,waveAmount do
+		local wave = waves[i]
+		wave.power = lerp(wave.power ,0, 1)
+--		wave.power = wave.power - dt -1
+		wave.x = wave.x +wave.speed * dt
+		if math.abs( wave.power) < 1 then
+	--		break
+		end
+		waves[i] = wave
+	end
+end
+
 function playerNearWaves()
-	return playerY-370 > 30
+	return playerY-380 > 20
 end
 
 function getWave(x)
-	return love.math.noise(x/300,love.timer.getTime()/2)
+	local height = love.math.noise(x/300,love.timer.getTime()/2)
+	
+	for i = 1, waveAmount do
+		local wave = waves[i]
+		print(wave.x )
+		if math.abs(wave.x - x) < wave.size then
+			local power = math.abs(wave.x -x)/wave.size 
+			power = power * power
+			power = power- 1
+			height = height + power
+			print(power)
+		end
+	end
+	
+	return height
 end 
